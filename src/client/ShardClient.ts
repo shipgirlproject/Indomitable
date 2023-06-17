5478e0ede532853618e674aadff312462fe00ebf
Deivu <davexdme@gmail.com>
2023-06-18 00:20:44 +0800

    fix: correctly inject custom identify throttler

--- src/client/ShardClient.ts
+++ src/client/ShardClient.ts
@@ -1,5 +1,5 @@
 import type { Client, ClientOptions as DiscordJsClientOptions } from 'discord.js';
-import type { WebSocketManager } from '@discordjs/ws';
+import { WebSocketManager, SimpleShardingStrategy, IShardingStrategy } from '@discordjs/ws';
 import { Indomitable } from '../Indomitable';
 import { ClientEvents, InternalEvents, LibraryEvents } from '../Util';
 import { ConcurrencyClient } from '../concurrency/ConcurrencyClient.js';
@@ -17,16 +17,37 @@ export class ShardClient {
         const clientOptions = manager.clientOptions as DiscordJsClientOptions || {};
         clientOptions.shards = process.env.INDOMITABLE_SHARDS!.split(' ').map(Number);
         clientOptions.shardCount = Number(process.env.INDOMITABLE_SHARDS_TOTAL);
+        // a very kekw way of injecting custom options, due to backward compatibility,
+        // d.js didn't provide a way to access the ws options of @discordjs/ws package
+        if (manager.handleConcurrency) {
+            if (!clientOptions.ws) clientOptions.ws = {};
+            if (!clientOptions.ws.buildStrategy) {
+                clientOptions.ws.buildStrategy = manager => {
+                    const strategy = new SimpleShardingStrategy(manager);
+                    manager.options.buildIdentifyThrottler = () => {
+                        const manager = new ConcurrencyClient(client.shard as unknown as ShardClientUtil);
+                        return Promise.resolve(manager);
+                    };
+                    return strategy;
+                };
+            } else {
+                // eslint-disable-next-line no-new-func
+                const clone = Function(clientOptions.ws.buildStrategy.toString()) as unknown as (manager: WebSocketManager) => IShardingStrategy ;
+                clientOptions.ws.buildStrategy = manager => {
+                    const strategy = clone(manager);
+                    manager.options.buildIdentifyThrottler = () => {
+                        const manager = new ConcurrencyClient(client.shard as unknown as ShardClientUtil);
+                        return Promise.resolve(manager);
+                    };
+                    return strategy;
+                };
+            }
+        }
         const client = new manager.client(clientOptions);
         // @ts-ignore -- our own class
         client.shard = new ShardClientUtil(manager, client);
         this.client = client;
         this.clusterId = Number(process.env.INDOMITABLE_CLUSTER);
-        if (!manager.handleConcurrency) return;
-        this.ws.options.buildIdentifyThrottler = () => {
-            const manager = new ConcurrencyClient(client.shard as unknown as ShardClientUtil);
-            return Promise.resolve(manager);
-        };
     }
 
     get ws(): WebSocketManager {
